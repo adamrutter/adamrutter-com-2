ab8909391c88dec4a8f48eb016b9de8534594126
Adam Rutter <adamrutter92@gmail.com>
2020-09-04 13:03:26 +0100

    About me section

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,21 @@
+import AboutMe from "../components/AboutMe"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import React from "react"
 import SEO from "../components/seo"
+import Section from "../components/Section"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <header>
-        <Hero />
+      <Hero />
     </header>
+    <main>
+      <Section>
+        <AboutMe />
+      </Section>
+    </main>
   </Layout>
 )
 
