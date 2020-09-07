f1fb1ed05825fae6f17fb87b6a12b6a555e5a2ef
Adam Rutter <adamrutter92@gmail.com>
2020-09-07 13:45:24 +0100

    Moved particles to own component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import { Container } from "react-bootstrap"
 import classNames from "classnames"
-import Particles from "react-particles-js"
+import Particles from "../Particles"
 import React from "react"
 import styles from "./Hero.module.scss"
 
@@ -13,70 +13,7 @@ const Hero = () => {
           Front end Web Developer/Designer
         </p>
       </div>
-      <Particles
-        className={classNames(styles.particles)}
-        params={{
-          particles: {
-            number: {
-              value: 50,
-            },
-            color: {
-              value: "#ffffff",
-            },
-            shape: {
-              type: "circle",
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 0.1,
-                opacity_min: 0.2,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 0.1,
-                size_min: 1,
-              },
-            },
-            line_linked: {
-              enable: true,
-              distance: 75,
-              color: "#ffffff",
-              opacity: 0.4,
-              width: 1,
-            },
-            move: {
-              enable: true,
-              speed: 0.5,
-              direction: "none",
-              out_mode: "out",
-            },
-          },
-          interactivity: {
-            detect_on: "window",
-            events: {
-              onhover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-            },
-          },
-          retina_detect: true,
-        }}
-      />
+      <Particles />
     </Container>
   )
 }
